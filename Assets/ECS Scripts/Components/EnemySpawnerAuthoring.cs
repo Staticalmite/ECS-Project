550d0ef295dc890296d8da616e24e0ad659f19d4
Staticalmite <146946883+Staticalmite@users.noreply.github.com>
2025-02-17 07:53:04 +0000

    Updated Spawners to work AGAIN

--- Assets/ECS Scripts/Components/EnemySpawnerAuthoring.cs
+++ Assets/ECS Scripts/Components/EnemySpawnerAuthoring.cs
@@ -3,22 +3,27 @@ using UnityEngine;
 
 public class EnemySpawnerAuthoring : MonoBehaviour
 {
-	public GameObject PrefabToSpawn;
-	public float SpawnDelay;
+    public GameObject PrefabToSpawn;
+    public float SpawnDelay;
 
-	private class EnemySpawnerBaker : Baker<EnemySpawnerAuthoring>
-	{
-		public override void Bake(EnemySpawnerAuthoring authoring)
-		{
-			Entity e = GetEntity(TransformUsageFlags.None);
+    private class EnemySpawnerBaker : Baker<EnemySpawnerAuthoring>
+    {
+        public override void Bake(EnemySpawnerAuthoring authoring)
+        {
+            //Convert it to an entity.
+            //'UsageFlags.None' means this won't move at spawn-time, as the spawner is static.
+            Entity e = GetEntity(TransformUsageFlags.None);
 
-			AddComponent(e, new EnemySpawnerComponent
-			{
-				PrefabToSpawn = GetEntity(authoring.PrefabToSpawn, TransformUsageFlags.Dynamic),
-				Timer = 0.0f,
-				SpawnDelay = authoring.SpawnDelay,
-				SpawnPosition = authoring.transform.position
-			});
-		}
-	}
+            AddComponent(e, new EnemySpawnerComponent
+            {
+                //Convert prefab into an entity, dynamic flag means it can move at runtime.
+                PrefabToSpawn = GetEntity(authoring.PrefabToSpawn, TransformUsageFlags.Dynamic),
+
+                //Set properties to the below values.
+                Timer = 0.0f,
+                SpawnDelay = authoring.SpawnDelay,
+                SpawnPosition = authoring.transform.position
+            });
+        }
+    }
 }
