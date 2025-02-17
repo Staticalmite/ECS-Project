f87bd69467c009d5f8e04bfd63c948aeb4c79b52
Staticalmite <146946883+Staticalmite@users.noreply.github.com>
2025-02-17 07:10:03 +0000

    Imported Blobs Upgrade

--- Assets/ECS Scripts/Components/EnemySpawnerAuthoring.cs
+++ Assets/ECS Scripts/Components/EnemySpawnerAuthoring.cs
@@ -10,16 +10,11 @@ public class EnemySpawnerAuthoring : MonoBehaviour
 	{
 		public override void Bake(EnemySpawnerAuthoring authoring)
 		{
-			//Convert it to an entity.
-			//'UsageFlags.None' means this won't move at spawn-time, as the spawner is static.
 			Entity e = GetEntity(TransformUsageFlags.None);
 
 			AddComponent(e, new EnemySpawnerComponent
 			{
-				//Convert prefab into an entity, dynamic flag means it can move at runtime.
 				PrefabToSpawn = GetEntity(authoring.PrefabToSpawn, TransformUsageFlags.Dynamic),
-
-				//Set properties to the below values.
 				Timer = 0.0f,
 				SpawnDelay = authoring.SpawnDelay,
 				SpawnPosition = authoring.transform.position
--- Assets/ECS Scripts/Systems/EnemySpawnerSystem.cs
+++ Assets/ECS Scripts/Systems/EnemySpawnerSystem.cs
@@ -32,32 +32,4 @@ public partial struct EnemySpawnerSystem : ISystem
 			spawner.ValueRW.Timer -= spawner.ValueRO.SpawnDelay;
 		}
 	}
-
-	private void UpdateSpawner(ref SystemState state, RefRW<EnemySpawnerComponent> spawner)
-	{
-		// Increment timer by delta
-		spawner.ValueRW.Timer += SystemAPI.Time.DeltaTime;
-
-		// If less than Spawn Rate timer, exit early.
-		if (spawner.ValueRO.Timer < spawner.ValueRO.SpawnDelay)
-			return;
-
-		// Otherwise, spawn an enemy and reset timer.
-		SpawnEnemy(ref state, spawner);
-		spawner.ValueRW.Timer = 0;
-	}
-
-	private void SpawnEnemy(ref SystemState state, RefRW<EnemySpawnerComponent> spawner)
-	{
-		// Create the entity, passing the prefab to spawn
-		Entity entity = state.EntityManager.Instantiate(spawner.ValueRO.PrefabToSpawn);
-
-		// Build local transform this object by making a localtransform component from a position
-		LocalTransform localTransform = LocalTransform.FromPosition(spawner.ValueRO.SpawnPosition);
-
-		//Then, set the entity's local transform to the transform we just built.
-		state.EntityManager.SetComponentData(entity, localTransform);
-
-	}
-
 }
